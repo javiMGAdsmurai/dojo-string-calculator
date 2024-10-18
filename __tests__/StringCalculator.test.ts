baa8b66e6f591be35282c24a38174961ab6a243b
Javier Gómez <javier.gomez@adsmurai.com>
2024-10-18 11:09:45 +0200

    Return single number

--- __tests__/StringCalculator.test.ts
+++ __tests__/StringCalculator.test.ts
@@ -8,4 +8,12 @@ describe('WHEN add without number', () => {
 
         expect(result).toBe(expected)
     });
+
+  test.each([{value: "4", expected: 4}, {value: "15", expected: 15}, {value: "25", expected: 25}, {value: "1", expected: 1}])
+    ('should return the number itself when there is only one', (value) => {
+
+    const result = StringCalculator.add(value.value)
+
+    expect(result).toBe(value.expected)
+  });
 });
